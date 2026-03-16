7bf7a08b4ac167c6c863567b9f553c6a18fbf022
bibizyano18 <artem.study18@gmail.com>
2026-03-16 15:03:21 +0300

    Исправил gitignore, исправил ошибку в html с type module в скрипте. Исправил путь папки для сборщика пакетов, добавил подпись коммитов

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var foo = 'bar';
+let foo = 'bar';
 foo = 'baz';
+console.log(foo);
 
 // prettier-ignore
-matrix = [
+let matrix = [
   1, 0, 0,
   0, 1, 0,
   0, 0, 1
@@ -10,4 +11,4 @@ matrix = [
 
 matrix = [10, 0, 0, 0, 1, 0, 0, 0, 1];
 
-document.getElementById('header').textContent = matrix;
+window.document.getElementById('header').textContent = matrix;
