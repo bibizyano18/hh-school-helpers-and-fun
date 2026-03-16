55a977b587a14ce07a663ce237f7750c6a5d83e0
bibizyano18 <artem.study18@gmail.com>
2026-03-16 15:30:24 +0300

    Поправил eslint.config, добавил все исправления по ts, исправил конфигурационные файлы

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from 'eslint/config';
 
+
 export default defineConfig([
-    {
-        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-        plugins: { js },
-        extends: ['js/recommended'],
-        languageOptions: { globals: globals.node },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
     },
-    tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_'
+      }]
+    }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ]);
